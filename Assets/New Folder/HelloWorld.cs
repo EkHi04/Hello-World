113f65c5cd1da039d5daa1cc51cbe7c0662a85ff
EkHi04 <160221030+EkHi04@users.noreply.github.com>
2024-02-23 08:58:47 +0100

    namespace

--- Assets/New Folder/HelloWorld.cs
+++ Assets/New Folder/HelloWorld.cs
@@ -1,7 +1,8 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-namespace HelloWorld{
+namespace SampleNameSpace
+{
     public class HelloWorld : MonoBehaviour
     {
         // Start is called before the first frame update
